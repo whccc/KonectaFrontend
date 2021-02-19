fc2b3391028c6e561db7dfe4433e32c7105d9875
Wilson Castro <wilsoncastro0710@gmail.com>
2021-02-18 20:11:51 -0500

    Main Project

---
+++ konecta/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hola</h1>
+    </div>
+  )
+}
---
+++ konecta/src/styles/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    background: 'red',
+    text: '#000',
+    primary: 'blue'
+  }
+}
